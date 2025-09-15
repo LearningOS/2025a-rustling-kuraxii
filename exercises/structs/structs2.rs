d688147062d2a45af1bc8bf0622c304669ea48e1
kuraxii <zj.zhu.cn@gmail.com>
2025-09-15 23:04:49 +0800

    2025/09/15

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -38,6 +38,7 @@ mod tests {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
         let your_order = Order{
+            name : "Hacker in Rust".to_string(),
             count: 1,
             ..order_template
         };
